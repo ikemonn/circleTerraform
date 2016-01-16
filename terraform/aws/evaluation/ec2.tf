88821c2281957d2c0c40f7f8014df0f43d0935f5
ikemonn <daiki.matsui@plaid.co.jp>
2016-01-16 12:30:24 +0900

    change

--- terraform/aws/evaluation/ec2.tf
+++ terraform/aws/evaluation/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "sample" {
     instance_type = "t2.micro"
     availability_zone = "ap-northeast-1a"
     tags {
-      Name = "terraform_sample"
+      Name = "terraform_sample_2"
     }
     vpc_security_group_ids = ["${var.default_security_group}"]
     key_name = "${var.lc_key_name}"
